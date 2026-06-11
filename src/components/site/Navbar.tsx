9ac31971f5ca340c91564136ef29950dcfe3ad23
Zemichael Tefera <135070468+zemuumez@users.noreply.github.com>
2026-06-11 03:40:47 +0300

    Revert "Add teachers section, assets and rotating headlines"
    This reverts commit cd4d0349c58e1618fefc141b9b64411d26ea3e1c.

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -51,7 +51,7 @@ export function Navbar() {
             className="text-[var(--grey-1200)]"
             onClick={() => setOpen(false)}
           >
-            <SiteLogo showName />
+            <SiteLogo />
           </Link>
 
           <nav className="hidden items-center gap-1 md:flex">
